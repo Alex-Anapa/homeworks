9e81e0a44f0aa4d1860b068eb241546ec3be690f
Alex-Anapa <alek.milkhert@yandex.ru>
2025-03-26 04:45:29 +0300

    Add files via upload

---
+++ california/js/main.js
@@ -0,0 +1,17 @@
+
+
+(function () {
+
+    var swiper = new Swiper('.welcome__swiper', {
+        pagination: {
+            el: '.swiper-pagination', 
+        clickable: true, 
+    },
+        navigation: {
+            nextEl: '.swiper-button-next', 
+            prevEl: '.swiper-button-prev', 
+    },
+});
+
+})();
+
